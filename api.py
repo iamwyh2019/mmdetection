3bd371c1d99b400bc9b1561f91363021198d303d
yuhengwu <799810767@qq.com>
2024-03-04 02:21:26 -0600

    Update api.py

--- api.py
+++ api.py
@@ -42,12 +42,15 @@ def parse_result(result: DetDataSample,
     pred_instances = pred_instances[:top_k]
 
     # convert to numpy
-    bboxes: np.ndarray = pred_instances.bboxes.cpu().numpy()
+    boxes: np.ndarray = pred_instances.bboxes.cpu().numpy()
     labels: np.ndarray = pred_instances.labels.cpu().numpy()
     scores: np.ndarray = pred_instances.scores.cpu().numpy()
     masks: np.ndarray = pred_instances.masks.cpu().numpy()
 
-    return masks, bboxes, labels, scores
+    # boxes need to be rounded
+    boxes = np.round(boxes).astype(np.int32)
+
+    return masks, boxes, labels, scores
 
 
 def process_image(image:np.ndarray,
@@ -57,15 +60,18 @@ def process_image(image:np.ndarray,
 
     # inference
     result = inference_detector(model, image)
-    masks, bboxes, labels, scores = parse_result(result, score_threshold, top_k)
+    masks, boxes, labels, scores = parse_result(result, score_threshold, top_k)
     masks = masks.astype(np.uint8)
 
     # get contours and geometry centers
     mask_contours = []
     geometry_center = []
     for mask in masks:
-        contour, _ = bitmap_to_polygon(mask)
-        mask_contours.append(contour)
+        contours, _ = bitmap_to_polygon(mask)
+        # find the largest contour
+        contours.sort(key=lambda x: len(x), reverse=True)
+        largest_contour = max(contours, key = cv2.contourArea)
+        mask_contours.append(largest_contour)
 
         # mask_crop is 0/1 matrix
         # the center is the center of mass (assume uniform density)
@@ -73,21 +79,139 @@ def process_image(image:np.ndarray,
         center = cv2.moments(mask)
         center_x = int(center["m10"] / center["m00"])
         center_y = int(center["m01"] / center["m00"])
-        geometry_center.append([center_x.item(), center_y.item()])
+        geometry_center.append([center_x, center_y])
 
-    return masks, mask_contours, bboxes, labels, scores, geometry_center
+    return masks, mask_contours, boxes, labels, scores, geometry_center
 
 
-# for frame in track_iter_progress((video_reader, len(video_reader)), task_name="frame"):
-#     result = inference_detector(model, frame, test_pipeline=test_pipeline)
-#     visualizer.add_datasample(
-#         name='video',
-#         image=frame,
-#         data_sample=result,
-#         draw_gt=False,
-#         show=False,
-#         pred_score_thr=args.score_thr)
-#     frame = visualizer.get_image()
+def get_recognition(image: np.ndarray,
+                    filter_objects: List[str] = [],
+                    score_threshold: float = 0.3,
+                    top_k: int = 15) -> Dict[str, Any]:
+    global CLASSES, COLORS
+
+    masks, mask_contours, boxes, labels, scores, geometry_center = process_image(image, score_threshold, top_k)
+
+    masks = masks.tolist()
+    mask_contours = [contour.tolist() for contour in mask_contours]
+    boxes = boxes.tolist()
+    scores = scores.tolist()
+    labels = labels.tolist()
+
+    # convert labels to class names
+    class_names = [CLASSES[label] for label in labels]
+
+    result = {
+        "masks": masks,
+        "mask_contours": mask_contours,
+        "boxes": boxes,
+        "scores": scores,
+        "labels": labels, # "labels" is the original label, "class_names" is the class name
+        "class_names": class_names,
+        "geometry_center": geometry_center
+    }
+
+    if filter_objects:
+        result = get_filtered_objects(result, filter_objects)
+
+    return result
+
+
+async def get_recognition_async(image: np.ndarray,
+                                filter_objects: List[str] = [],
+                                score_threshold: float = 0.3,
+                                top_k: int = 15) -> Dict[str, Any]:
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(executor, get_recognition, image, filter_objects, score_threshold, top_k)
+
+
+def draw_recognition(image: np.ndarray, result: Dict[str, Any],
+                     black: bool = False, draw_contour: bool = False, draw_mask: bool = True, 
+                     draw_box: bool = False, draw_text: bool = True, draw_score = True,
+                     alpha: float = 0.45) -> np.ndarray:
+    masks = result['masks']
+    mask_contours = result['mask_contours']
+    boxes = result['boxes']
+    class_names = result['class_names']
+    scores = result['scores']
+    geometry_center = result['geometry_center']
+    labels = result['labels']
+    
+    if black:
+        image = np.zeros_like(image)
+
+    if len(masks) == 0:
+        return image
+
+    # colors
+    # each color is a 3-tuple (B, G, R)
+    colors = []
+    color_list = []
+    for label in labels:
+        color = COLORS[label]
+        color = (color[2], color[1], color[0])
+        color_list.append(color)
+        colors.append(np.array(color, dtype=float).reshape(1,1,1,3))
+    colors = np.concatenate(colors, axis=0)
+    
+    if draw_mask:
+        # masks N*H*W
+        masks = np.array(masks, dtype=float)
+        # change to N*H*W*1
+        masks = np.expand_dims(masks, axis=3)
+
+        masks_color = masks.repeat(3, axis=3) * colors * alpha
+
+        inv_alpha_masks = masks * (-alpha) + 1
+
+        masks_color_summand = masks_color[0]
+        if len(masks_color) > 1:
+            inv_alpha_cumul = inv_alpha_masks[:-1].cumprod(axis=0)
+            masks_color_cumul = masks_color[1:] * inv_alpha_cumul
+            masks_color_summand += masks_color_cumul.sum(axis=0)
+
+        image = image * inv_alpha_masks.prod(axis=0) + masks_color_summand
+        image = image.astype(np.uint8)
+
+    # draw the contours
+    if draw_contour:
+        for i, contour in enumerate(mask_contours):
+            contour = np.array(contour, dtype=np.int32)
+            color = color_list[i]
+            cv2.drawContours(image, [contour], -1, color, 2)
+
+    # draw box
+    if draw_box:
+        for i, box in enumerate(boxes):
+            x1, y1, x2, y2 = box
+            color = color_list[i]
+            cv2.rectangle(image, (x1, y1), (x2, y2), color, 1)
+
+    # place text at the center
+    if draw_text:
+        for i, center in enumerate(geometry_center):
+            text = class_names[i]
+            if draw_score:
+                text += f' {scores[i]:.2f}'
+            cv2.putText(image, text, center, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    
+    return image
+
+
+def get_filtered_objects(result: Dict[str, Any], filter_objects: List[str]) -> Dict[str, Any]:
+    """Filter the objects by class names."""
+    assert 'class_names' in result
+
+    fields = result.keys()
+    new_result = {field: [] for field in fields}
+    class_names = result['class_names']
+
+    for i, class_name in enumerate(class_names):
+        if class_name in filter_objects:
+            for field in fields:
+                new_result[field].append(result[field][i])
+
+    return new_result
 
 
 def bitmap_to_polygon(bitmap):
@@ -117,3 +241,16 @@ def bitmap_to_polygon(bitmap):
     with_hole = (hierarchy.reshape(-1, 4)[:, 3] >= 0).any()
     contours = [c.reshape(-1, 2) for c in contours]
     return contours, with_hole
+
+
+def main():
+    image = cv2.imread('demo/demo.jpg')
+    result = get_recognition(image)
+    image = draw_recognition(image, result, draw_contour=True, 
+                             draw_mask=True, draw_box=False, 
+                             draw_text=True, draw_score=False, alpha=0.45)
+    cv2.imshow('result', image)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
