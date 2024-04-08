8d126a50edfe63e96d6599606255a1e67004194c
yuhengwu <799810767@qq.com>
2024-04-07 21:29:48 -0500

    Incorporate mmdeploy into api

--- api_deploy.py
+++ api_deploy.py
@@ -11,59 +11,106 @@ from typing import List, Dict, Callable, Any, Union, Tuple
 from mmdet.structures import DetDataSample
 import os
 
-model_name = 'rtmdet-ins_l_8xb32-300e_coco'
+from mmdeploy_runtime import Detector
 
-config_path = os.path.join(os.path.dirname(__file__), f'configs/rtmdet/{model_name}.py')
-checkpoint_path = os.path.join(os.path.dirname(__file__), f'checkpoints/{model_name}.pth')
-device = 'cuda:0'
+# tiny, s, l
+model_size = 'l'
 
-# build the model from a config file and a checkpoint file
-model = init_detector(config_path, checkpoint_path, device=device)
+METAINFO = {
+        'classes':
+        ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train',
+         'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+         'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+         'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+         'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+         'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+         'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
+         'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+         'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+         'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+         'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+         'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+         'scissors', 'teddy bear', 'hair drier', 'toothbrush'),
+        # palette is a list of color tuples, which is used for visualization.
+        'palette':
+        [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
+         (0, 60, 100), (0, 80, 100), (0, 0, 70), (0, 0, 192), (250, 170, 30),
+         (100, 170, 30), (220, 220, 0), (175, 116, 175), (250, 0, 30),
+         (165, 42, 42), (255, 77, 255), (0, 226, 252), (182, 182, 255),
+         (0, 82, 0), (120, 166, 157), (110, 76, 0), (174, 57, 255),
+         (199, 100, 0), (72, 0, 118), (255, 179, 240), (0, 125, 92),
+         (209, 0, 151), (188, 208, 182), (0, 220, 176), (255, 99, 164),
+         (92, 0, 73), (133, 129, 255), (78, 180, 255), (0, 228, 0),
+         (174, 255, 243), (45, 89, 255), (134, 134, 103), (145, 148, 174),
+         (255, 208, 186), (197, 226, 255), (171, 134, 1), (109, 63, 54),
+         (207, 138, 255), (151, 0, 95), (9, 80, 61), (84, 105, 51),
+         (74, 65, 105), (166, 196, 102), (208, 195, 210), (255, 109, 65),
+         (0, 143, 149), (179, 0, 194), (209, 99, 106), (5, 121, 0),
+         (227, 255, 205), (147, 186, 208), (153, 69, 1), (3, 95, 161),
+         (163, 255, 0), (119, 0, 170), (0, 182, 199), (0, 165, 120),
+         (183, 130, 88), (95, 32, 0), (130, 114, 135), (110, 129, 133),
+         (166, 74, 118), (219, 142, 185), (79, 210, 114), (178, 90, 62),
+         (65, 70, 15), (127, 167, 115), (59, 105, 106), (142, 108, 45),
+         (196, 172, 0), (95, 54, 80), (128, 76, 255), (201, 57, 1),
+         (246, 0, 122), (191, 162, 208)]
+    }
 
-CLASSES = model.dataset_meta['classes']
-COLORS = model.dataset_meta['palette']
+CLASSES = METAINFO['classes']
+COLORS = METAINFO['palette']
+
+detector = Detector(
+    model_path=os.path.join('sdk', f'rtmdet-{model_size}-sdk'),
+    device_name='cuda',
+    device_id=0
+)
 
 executor = ThreadPoolExecutor(max_workers = 30)
 
-def parse_result(result: DetDataSample,
+# result is a tuple of three elements: boxes, labels, masks
+# boxes: np.ndarray, N*5, each row is (x1, y1, x2, y2, score)
+# labels: np.ndarray, N, each element is the class index
+# masks: List[np.ndarray], N, each element is a binary mask
+def parse_result(result: Tuple[np.ndarray, np.ndarray, List[np.ndarray]],
                  score_threshold: float = 0.15,
-                 top_k: int = 15) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+                 top_k: int = 15) -> Tuple[List[np.ndarray], np.ndarray, np.ndarray, np.ndarray]:
     """Parse the detection results."""
-    pred_instances = result.pred_instances
+    bboxes, labels, masks = result
+    # break the boxes into [x1, y1, x2, y2], score
+    boxes: np.ndarray = bboxes[:, 0:4]
+    scores: np.ndarray = bboxes[:, 4]
 
     # first, filter out the low score boxes
-    valid_inds = pred_instances.scores >= score_threshold
-    pred_instances = pred_instances[valid_inds]
+    valid_inds = scores >= score_threshold
+    boxes = boxes[valid_inds]
+    labels = labels[valid_inds]
+    scores = scores[valid_inds]
+    masks = [masks[i] for i in range(len(masks)) if valid_inds[i]]
 
-    # sort the scores
-    scores = pred_instances.scores
-    _, sort_inds = scores.sort(descending=True)
-    pred_instances = pred_instances[sort_inds]
-
-    # get the top_k
-    pred_instances = pred_instances[:top_k]
-
-    # convert to numpy
-    boxes: np.ndarray = pred_instances.bboxes.cpu().numpy()
-    labels: np.ndarray = pred_instances.labels.cpu().numpy()
-    scores: np.ndarray = pred_instances.scores.cpu().numpy()
-    masks: np.ndarray = pred_instances.masks.cpu().numpy()
+    # get the results of the top_k scores
+    if len(scores) > top_k:
+        top_inds = np.argsort(-scores)[:top_k]
+        boxes = boxes[top_inds]
+        labels = labels[top_inds]
+        scores = scores[top_inds]
+        masks = [masks[i] for i in top_inds]
 
     # boxes need to be rounded
     boxes = np.round(boxes).astype(np.int32)
+
+    # masks are 0/255, convert to 0/1
+    masks = [mask // 255 for mask in masks]
 
     return masks, boxes, labels, scores
 
 
 def process_image(image:np.ndarray,
                   score_threshold: float = 0.3,
-                  top_k: int = 15) -> Tuple[np.ndarray, List[np.ndarray], np.ndarray, np.ndarray, np.ndarray, List[List[int]]]:
-    global model
+                  top_k: int = 15) -> Tuple[List[np.ndarray], List[np.ndarray], np.ndarray, np.ndarray, np.ndarray, List[List[int]]]:
+    global detector
 
     # inference
-    result = inference_detector(model, image)
+    result = detector(image)
     masks, boxes, labels, scores = parse_result(result, score_threshold, top_k)
-    masks = masks.astype(np.uint8)
 
     # get contours and geometry centers
     mask_contours = []
@@ -270,9 +317,14 @@ def main():
     image = cv2.imread('demo/large_image.jpg')
 
     result = get_recognition(image)
-    image = async_draw_recognition(image, result, draw_contour=True, draw_text=True, draw_score=True)
+    image = async_draw_recognition(image, result,
+                                   draw_contour=True,
+                                   draw_text=True,
+                                   draw_score=True,
+                                   draw_mask=True,)
     cv2.imshow('result', image)
     cv2.waitKey(0)
+
 
 if __name__ == '__main__':
     main()
